3878550125466d85375ca45f0da755a5259c96a2
pmammino <paul.mammino@wagner.edu>
2020-11-23 19:57:36 -0500

    Update script.py

--- models/script.py
+++ models/script.py
@@ -2,7 +2,7 @@ import http.client
 import json
 import pandas as pd
 
-from src.common.database import Database
+from common.database import Database
 Database.initialize()
 conn = http.client.HTTPSConnection("api.sportradar.us")
 
