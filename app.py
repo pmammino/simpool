cce645c4e27220ce1f9bc8e9bbe2e55ce0d86af0
pmammino <paul.mammino@wagner.edu>
2021-09-10 12:59:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def edit_lineup(lineup_id):
 
 @application.before_first_request
 def initialize_database():
-    user_address = request.environ['REMOTE_ADDR']
+    user_address = request.headers['X-Real-IP']
     response = json.loads(requests.get("http://api.ipstack.com/"+user_address+"?access_key=2fe74d1492a0ae71f6423ec9150b3a08&fields=region_code&output=json").text)["region_code"]
     session["location"] = user_address
     Database.initialize()
